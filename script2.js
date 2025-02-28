19de4397cc69916611be63795ae04523a1df14bb
and86rey <125742554+and86rey@users.noreply.github.com>
2025-02-28 16:40:02 +0300

    script2.js

---
+++ script2.js
@@ -0,0 +1,11 @@
+package com.market.risk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarketRiskApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MarketRiskApplication.class, args);
+    }
+}
